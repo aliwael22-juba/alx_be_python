1f89b544b347c1fee7269dc600a16f145080dfcc
Ali Wael <aliabdelghany511@gmail.com>
2025-07-16 02:21:08 +0300

    Creating a python script that performs basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+print("Addition of 10 and 5 is", result) 
+result = number1 - number2
+print("Subtraction of 10 and 5 is", result)
+result = number1 * number2
+print("Multiplication of 10 and 5 is", result)
