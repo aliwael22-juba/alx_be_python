bd4af3bcdc208b1f153c2a18ca2322bcc24b5dbc
Ali Wael <aliabdelghany511@gmail.com>
2025-08-16 18:20:05 +0300

    create arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation():
+def perform_operation(num1, num2, operation):
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation ('add', 'subtract', 'multiply', 'divide'): ").strip().lower()  
@@ -18,4 +18,4 @@ def perform_operation():
         print("Error: Invalid operation.") 
     print(f"The result of {operation}ing {num1} and {num2} is: {result}") 
 
-perform_operation()
+perform_operation("num1","num2","operation")
