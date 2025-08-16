5657ffe851bc5751e07448bb6b68eff1369adf60
Ali Wael <aliabdelghany511@gmail.com>
2025-08-16 18:23:18 +0300

    create arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,11 +9,11 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         result = num1 * num2 
     elif operation == 'divide':
-        if num2 != 0:
-            result = num1 / num2
+        if num2 == 0:
+              print("Error: Division by zero is not allowed.")
+              return
         else:
-            print("Error: Division by zero is not allowed.")
-            return
+            result = num1 / num2
     else:
         print("Error: Invalid operation.") 
     print(f"The result of {operation}ing {num1} and {num2} is: {result}") 
